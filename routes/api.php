c46122e6e185400506cc22534eead82161be6801
ohiane irure solchaga <ohianegares@hotmail.com>
2025-03-29 12:24:52 +0100

    quitado validacion para pruebas

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::get('ofertas', [OfertaController::class, 'index']);
 Route::post('/registro', [AuthController::class, 'registro']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::middleware(['auth:sanctum',VerificarValidacion::class])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/titulos/activos', [TituloController::class,'titulosActivos'])->middleware(['ability:administrador,demandante,empresa']);//pueden acceder los tres roles
     Route::post('/titulos/demandante', [TituloController::class,'agregarTitulos'])->middleware(['ability:demandante']);//pueden acceder solo los demandantes
     Route::get('titulos/demandante',[TituloController::class,'titulosDemandante'])->middleware(['ability:demandante']);
